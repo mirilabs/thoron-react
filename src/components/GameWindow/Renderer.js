3f6ce39e680832f2995f64a707261a99731125ae
miri <49813832+mirilabs@users.noreply.github.com>
2023-05-25 15:31:30 -0400

    feature: grid drawing

--- src/components/GameWindow/Renderer.js
+++ src/components/GameWindow/Renderer.js
@@ -1,11 +1,72 @@
+import defaults from "./defaults";
+
 class Renderer {
-  constructor(canvasRef, opts) {
+  /**
+   * Draws game state onto a canvas
+   * @param {thoron.Chapter} chapter Chapter instance
+   * @param {React.Ref} canvasRef Reference to the target <canvas> element
+   * @param {Object} opts
+   * @param {string} opts.background URL to the background image
+   * @param {number} opts.tileWidth Width of a single tile, in pixels
+   * @param {number} opts.tileHeight Height of a single tile, in pixels
+   * @param {Object} ctxOpts Properties of this object will be assigned to the
+   *  CanvasRenderingContext2D used for drawing (`lineWidth`, `font`, etc.)
+   */
+  constructor(chapter, canvasRef, {
+    background,
+    tileWidth = defaults.TILE_SIZE,
+    tileHeight = defaults.TILE_SIZE,
+    ctxOpts = {
+      lineWidth: 1
+    }
+  }) {
     this.canvasRef = canvasRef;
     this.canvas = canvasRef.current;
+    this.ctx = this.canvas.getContext('2d');
+
+    this.chapter = chapter;
+
+    this.background = background;
+    this.tileWidth = tileWidth;
+    this.tileHeight = tileHeight;
+    Object.assign(this.ctx, ctxOpts);
+  }
+
+  get width() {
+    return this.canvas.width;
+  }
+
+  get height() {
+    return this.canvas.height;
   }
 
   draw() {
+    this.drawBackground();
+    this.drawGrid();
+  }
+
+  drawBackground() {
+    if (!this.background) { return }
+
+    let img = new Image();
+    img.src = this.background;
+    this.ctx.drawImage(img, 0, 0);
+  }
+
+  drawGrid() {
+    let ctx = this.ctx;
+    let x, y;
     
+    ctx.beginPath();
+    for (x = 0; x <= this.width; x += this.tileWidth) {
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, this.height);
+    }
+    for (y = 0; y <= this.height; y += this.tileHeight) {
+      ctx.moveTo(0, y);
+      ctx.lineTo(this.width, y);
+    }
+    ctx.stroke();
   }
 }
 
--- src/components/GameWindow/index.js
+++ src/components/GameWindow/index.js
@@ -1,27 +1,27 @@
 import React, { useContext, useEffect, useRef } from 'react';
 import { ThoronContext } from "../../utils/ThoronContext";
 import Renderer from './Renderer.js'
+import defaults from './defaults.js';
 
-function GameWindow(props) {
-  const {
-    background,
-    tileWidth = 32,
-    tileHeight = 32
-  } = props;
-
+function GameWindow({ rendererOpts = {} }) {
   const chapter = useContext(ThoronContext);
-  
-  // determine canvas size
-  let { width, height } = chapter.terrain.grid;
-  width = width * tileWidth;
-  height = height * tileHeight;
-  
   const canvasRef = useRef(null);
-  const renderer = new Renderer(canvasRef, props);
-  renderer.draw();
+  
+  // determine size of <canvas>
+  let { width, height } = chapter.terrain.grid;
+  width = width * (rendererOpts.tileWidth || defaults.TILE_SIZE);
+  height = height * (rendererOpts.tileHeight || defaults.TILE_SIZE);  
+
+  // setup renderer
+  useEffect(() => {
+    if (canvasRef.current) {
+      const renderer = new Renderer(chapter, canvasRef, rendererOpts);
+      renderer.draw();
+    }
+  }, [chapter, canvasRef, rendererOpts])
 
   return (
-    <canvas className="thoron__map-view" ref={canvasRef}
+    <canvas className="thoron__renderer" ref={canvasRef}
       {...{ width, height }} />
   )
 }
