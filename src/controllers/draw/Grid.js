d7e4969f2046406606855285b24149ab0a3f5671
miri <49813832+mirilabs@users.noreply.github.com>
2023-07-06 01:57:34 +0900

    split Grid from Background

---
+++ src/controllers/draw/Grid.js
@@ -0,0 +1,24 @@
+import DrawnEntity from './DrawnEntity';
+
+class Grid extends DrawnEntity {
+  constructor(width, height, tileWidth, tileHeight) {
+    super(0, 0, width, height);
+    this.tileWidth = tileWidth;
+    this.tileHeight = tileHeight;
+  }
+
+  draw(ctx) {
+    let x, y;
+    
+    ctx.beginPath();
+    for (x = 0; x <= this.width; x += this.tileWidth) {
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, this.height);
+    }
+    for (y = 0; y <= this.height; y += this.tileHeight) {
+      ctx.moveTo(0, y);
+      ctx.lineTo(this.width, y);
+    }
+    ctx.stroke();
+  }
+}
