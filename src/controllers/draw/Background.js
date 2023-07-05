5a03f292a30ae64e10b018921455320051c5a2f7
yjn <yoon512+1@gmail.com>
2023-07-06 01:57:34 +0900

    split Grid from Background

--- src/controllers/draw/Background.js
+++ src/controllers/draw/Background.js
@@ -1,12 +1,10 @@
 import Sprite from './Sprite';
 
 class Background extends Sprite {
-  constructor(src, width, height, tileWidth, tileHeight) {
+  constructor(src, width, height) {
     super(src, 0, 0, width, height);
 
     this.src = src;
-    this.tileWidth = tileWidth;
-    this.tileHeight = tileHeight;
     this.z = -1
   }
 
@@ -14,23 +12,6 @@ class Background extends Sprite {
     if (this.image != null) {
       ctx.drawImage(this.image, 0, 0, this.width, this.height);
     }
-
-    this._drawGrid(ctx);
-  }
-
-  _drawGrid(ctx) {
-    let x, y;
-    
-    ctx.beginPath();
-    for (x = 0; x <= this.width; x += this.tileWidth) {
-      ctx.moveTo(x, 0);
-      ctx.lineTo(x, this.height);
-    }
-    for (y = 0; y <= this.height; y += this.tileHeight) {
-      ctx.moveTo(0, y);
-      ctx.lineTo(this.width, y);
-    }
-    ctx.stroke();
   }
 }
 
