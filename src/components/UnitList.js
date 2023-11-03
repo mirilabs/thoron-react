9c868dc9d1cc38f313da715d20ca416366aa64f9
yjn0 <49813832+yjn0@users.noreply.github.com>
2023-11-03 18:46:46 +0000

    add <UnitList>

---
+++ src/components/UnitList.js
@@ -0,0 +1,35 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { ThoronContext } from './ThoronContext';
+import useEventListener from './useEventListener';
+
+function UnitList({ render }) {
+  const { chapter, uiEvents } = useContext(ThoronContext);
+
+  const [units, setUnits] = useState(null);
+  useEffect(() => {
+    setUnits(chapter.units.map(unit => unit.serialize()));
+  }, []);
+  
+  const [selected, setSelected] = useState(null);
+  useEventListener(uiEvents, 'select_unit', unit => {
+    setSelected(unit.id);
+  });
+
+  const data = {
+    units,
+    selected
+  }
+
+  if (render !== undefined) {
+    return render(data);
+  }
+  else {
+    return (
+      <div>
+        {JSON.stringify(data)}
+      </div>
+    )
+  }
+}
+
+export default UnitList;
