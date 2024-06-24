18fb7e1c8a4a133e8410ddd18c2b566e73dc8dbe
miri <49813832+mirilabs@users.noreply.github.com>
2024-06-24 07:30:47 -0400

    add rect utility class

---
+++ src/engine/utils/Rect.ts
@@ -0,0 +1,38 @@
+import { IPosition, IRectangle } from "../components";
+
+class Rect {
+    pos: IPosition;
+    rect: IRectangle;
+
+    constructor(pos: IPosition, rect: IRectangle) {
+        this.pos = pos;
+        this.rect = rect;
+    }
+
+    left(): number {
+        return this.pos.x;
+    }
+
+    top(): number {
+        return this.pos.y;
+    }
+
+    right(): number {
+        return this.pos.x + this.rect.width;
+    }
+
+    bottom(): number {
+        return this.pos.y + this.rect.height;
+    }
+
+    collidePoint(p: IPosition): boolean {
+        let { x, y } = p;
+
+        return x > this.left() &&
+            x < this.right() &&
+            y > this.top() &&
+            y < this.bottom();
+    }
+}
+
+export default Rect;
