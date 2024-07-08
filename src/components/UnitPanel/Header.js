e8a434d78ad80a32c6033748dfa8416994e76849
miri <49813832+mirilabs@users.noreply.github.com>
2024-07-08 18:59:21 -0400

    feat: clicking name in unitPanel toggles detail view

--- src/components/UnitPanel/Header.js
+++ src/components/UnitPanel/Header.js
@@ -1,24 +1,28 @@
+import { useUIEmitter } from "components/utils/useUIAction";
 import "./Header.scss";
 import React from "react";
 
 function UnitPanelHeader({ unit }) {
+  const toggleDetail = useUIEmitter('toggle_character_detail_display');
+    
   if (!unit) return null;
-
-  const portrait = unit.record.sprite,
-    name = unit.record.name ?? 'Unit',
-    equipped = unit.items[unit.state.equippedIndex] ?? { name: '---' };
-
-  return (
-    <div className="unit-panel__header">
-      <span className="unit-selector">
-        <img className="sprite" src={portrait} alt={name} />
-        <h1 className="name">{name}</h1>
-      </span>
-      <span className="weapon-selector">
-        <h2 className="equipped">{equipped.name ?? '???'}</h2>
-      </span>
-    </div>
-  );
+  else {
+    const portrait = unit.record.sprite,
+      name = unit.record.name ?? 'Unit',
+      equipped = unit.items[unit.state.equippedIndex] ?? { name: '---' };
+    
+    return (
+      <div className="unit-panel__header">
+        <span className="unit-selector" onClick={toggleDetail}>
+          <img className="sprite" src={portrait} alt={name} />
+          <h1 className="name">{name}</h1>
+        </span>
+        <span className="weapon-selector">
+          <h2 className="equipped">{equipped.name ?? '???'}</h2>
+        </span>
+      </div>
+    );
+  }
 }
 
 export default UnitPanelHeader;
