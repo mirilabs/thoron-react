a385c3e193798b3c2fd1dc9abaeba9376e7d6850
miri <49813832+mirilabs@users.noreply.github.com>
2024-07-26 21:35:58 -0400

    add item icons

---
+++ src/icons/README.md
@@ -0,0 +1,4 @@
+# icons
+
+Vectors and icons by [SVG Repo](https://www.svgrepo.com)
+Distributed under one of [these licenses](https://www.svgrepo.com/page/licensing)
---
+++ src/icons/items/index.ts
@@ -0,0 +1,19 @@
+import sword from "./sword.svg";
+import lance from "./lance.svg";
+import axe from "./axe.svg";
+import tome from "./tome.svg";
+import knife from "./knife.svg";
+import bow from "./bow.svg";
+import unknown from "./unknown.svg";
+
+const icons = {
+  sword,
+  lance,
+  axe,
+  tome,
+  knife,
+  bow,
+  unknown
+}
+
+export default icons;
