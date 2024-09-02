155f5291ba5c8c2b9fecf085ddda38eb21ae6b0f
miri <49813832+mirilabs@users.noreply.github.com>
2024-09-02 02:04:45 -0400

    feat: MotionPath

--- src/engine/utils/Vector2.ts
+++ src/engine/utils/Vector2.ts
@@ -4,32 +4,70 @@ class Vector2 implements IVector2 {
   x: number;
   y: number;
 
-  static eq(left: IVector2, right: IVector2) {
+  static copy(v: IVector2) {
+    return {
+      x: v.x,
+      y: v.y
+    }
+  }
+
+  static eq(left: IVector2, right: IVector2): boolean {
     return left.x === right.x && left.y === right.y;
   }
 
-  static sum(left: IVector2, right: IVector2) {
+  static sum(left: IVector2, right: IVector2): IVector2 {
     return {
       x: left.x + right.x,
       y: left.y + right.y
     }
   }
 
-  static difference(left: IVector2, right: IVector2) {
+  static difference(left: IVector2, right: IVector2): IVector2 {
     return {
       x: left.x - right.x,
       y: left.y - right.y
     }
   }
 
-  static scale(v: IVector2, scale: number) {
+  static lengthSquared(v: IVector2): number {
+    return v.x ** 2 + v.y ** 2;
+  }
+
+  static length(v: IVector2): number {
+    return Math.sqrt(this.lengthSquared(v));
+  }
+
+  static distanceSquared(left: IVector2, right: IVector2): number {
+    let d = this.difference(left, right);
+    return this.lengthSquared(d);
+  }
+
+  static distance(left: IVector2, right: IVector2): number {
+    let d = this.difference(left, right);
+    return this.length(d);
+  }
+
+  static normalize(v: IVector2): IVector2 {
+    let scale = 1 / this.length(v);
+    return this.scale(v, scale);
+  }
+
+  static scale(v: IVector2, scale: number): IVector2 {
     return {
       x: v.x * scale,
       y: v.y * scale
     }
   }
 
-  static rotateAboutOrigin(v: IVector2, theta: number) {
+  static lerp(v1: IVector2, v2: IVector2, fraction: number) {
+    let d = this.difference(v2, v1);
+    return {
+      x: v1.x + (d.x * fraction),
+      y: v1.y + (d.y * fraction)
+    }
+  }
+
+  static rotateAboutOrigin(v: IVector2, theta: number): IVector2 {
     let { x, y } = v;
     let sinT = Math.sin(theta);
     let cosT = Math.cos(theta);
