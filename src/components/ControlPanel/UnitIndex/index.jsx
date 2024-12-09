04c9943da8be2c4e5975af78fb5f074f04a347a9
miri <49813832+mirilabs@users.noreply.github.com>
2024-12-09 21:29:41 +0000

    feat: UnitIndex

---
+++ src/components/ControlPanel/UnitIndex/index.jsx
@@ -0,0 +1,34 @@
+import ThoronContext from "components/ThoronContext";
+import { useSelectedUnit } from "components/utils/useUnit";
+import React, { useContext } from "react";
+import UnitDetail from "../UnitDetail";
+import UnitTile from "./UnitTile";
+
+function UnitIndexContainer() {
+  const selectedUnit = useSelectedUnit();
+
+  if (selectedUnit) {
+    return <UnitDetail unit={selectedUnit} />
+  }
+  else {
+    return <UnitIndex />
+  }
+}
+
+function UnitIndex() {
+  const { chapter } = useContext(ThoronContext);
+  const units = chapter.getUnits();
+
+  const tiles = units.map(unit => (
+    <UnitTile key={unit.id} unit={unit} />
+  ));
+
+  return (
+    <>
+      {tiles}
+    </>
+  )
+
+}
+
+export default UnitIndexContainer;
