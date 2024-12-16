2adb90c074418299bac3ee6eda09c317961fbbb5
miri <49813832+mirilabs@users.noreply.github.com>
2024-12-16 19:36:23 +0000

    add placeholder ChapterMenu, ChapterLog

---
+++ src/components/ControlPanel/ChapterMenu/index.tsx
@@ -0,0 +1,19 @@
+import React, { useContext }  from "react";
+import "./ChapterMenu.scss";
+import ThoronContext from "components/ThoronContext";
+
+function ChapterMenu() {
+  const { controller } = useContext(ThoronContext);
+
+  const handleEndTurn = () => {
+    // TODO
+  }
+
+  return (
+    <div className="chapter-menu" onClick={handleEndTurn}>
+      <button>End Turn</button>
+    </div>
+  )
+}
+
+export default ChapterMenu;
