37eb9c2c1a4ecd83a35e26d05dd4da5b6a9f95ac
miri <49813832+mirilabs@users.noreply.github.com>
2024-12-19 21:02:56 +0000

    feat: damage number animation

--- src/game/entities/MotionSystem/AttackMotion.ts
+++ src/game/entities/MotionSystem/AttackMotion.ts
@@ -3,6 +3,7 @@ import UnitBody from "../UnitBody";
 import { AttackEvent } from "thoron";
 import MotionSequence from "engine/utils/MotionSequence";
 import Vector2 from "engine/utils/Vector2";
+import DamageNumber from "../ui/DamageNumber";
 
 const MOTION_SCALE = 0.3; // 1.0 = one tile width
 const FORWARD_MOTION_TIME = 120;  // ms
@@ -38,6 +39,17 @@ function AttackMotion(
   path.moveTo(startingPos, BACKWARD_MOTION_TIME);
 
   return async function attackMotion() {
+    window.setTimeout(() => {
+      let origin = Vector2.scale({
+        x: game.config.tileWidth,
+        y: game.config.tileHeight
+      }, 0.5);
+      origin = origin.sum(target.components.position);
+
+      const damageNumber = new DamageNumber(origin, event, game.config);
+      damageNumber.addToScene(game.scene);
+    }, FORWARD_MOTION_TIME);
+
     await path.start();
     attacker.resetPosition();
     await wait(POST_ATTACK_DELAY_TIME);
