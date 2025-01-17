9bf730c1933681701ba9ffa08347c6e05837c017
miri <49813832+mirilabs@users.noreply.github.com>
2025-01-16 23:29:03 -0500

    feat: CombatLog detail includes attacks/HP values

---
+++ src/components/ControlPanel/ChapterLog/CombatLog/DefeatIcon.tsx
@@ -0,0 +1,8 @@
+import "./DefeatIcon.scss";
+import React from 'react';
+
+function DefeatIcon() {
+  return <i className="fas fa-x defeat-icon" />
+}
+
+export default DefeatIcon;
--- src/components/ControlPanel/ChapterLog/CombatLog/index.tsx
+++ src/components/ControlPanel/ChapterLog/CombatLog/index.tsx
@@ -1,7 +1,15 @@
 import "./CombatLog.scss";
 import React, { useState } from "react";
-import { ActionResult, DeployedUnit } from "thoron";
-import Forecast from "./Forecast";
+import {
+  ActionResult,
+  DeployedUnit,
+  ICombatForecastSide,
+  ICombatResultSide
+} from "thoron";
+import CombatEvents from "./CombatEvents";
+import CombatPreview, {
+  ICombatPreview
+} from "components/gameView/CombatMenu/CombatPreview";
 
 function CombatLog({ unit, target, result }: {
   unit: DeployedUnit,
@@ -24,15 +32,56 @@ function CombatLog({ unit, target, result }: {
   )
 }
 
+function getForecastData(
+  unit: DeployedUnit,
+  forecast: ICombatForecastSide,
+  result: ICombatResultSide
+): ICombatPreview {
+  return {
+    ...forecast,
+    unitName: unit.record.name,
+    unitSpriteUrl: unit.record['sprite'],
+    equippedItem: unit.items[unit.state.equippedIndex],
+    maxHP: unit.getStats().mhp,
+    startHP: result.startHP,
+    endHP: result.endHP
+  } as ICombatPreview;
+}
+
 function CombatLogDetail({ unit, target, result }: {
   unit: DeployedUnit,
   target: DeployedUnit,
   result: ActionResult
 }) {
+  let left = getForecastData(
+    unit,
+    result.combatForecast.initiator,
+    result.combatResult.initiator
+  );
+  let right = getForecastData(
+    target,
+    result.combatForecast.defender,
+    result.combatResult.defender
+  );
+  
+  const renderInitiatorOnLeft = unit.getTeam() < target.getTeam();
+  if (!renderInitiatorOnLeft) {
+    // swap positions
+    let temp = left;
+    left = right;
+    right = temp;
+  }
 
   return (
     <div className="combat-detail">
-      <Forecast unit={unit} target={target} data={result.forecast} />
+      <CombatPreview data={left} />
+      <CombatEvents
+        events={result.events}
+        leftUnit={renderInitiatorOnLeft ? unit : target}
+        rightUnit={renderInitiatorOnLeft ? target : unit}
+        leftData={left}
+        rightData={right} />
+      <CombatPreview data={right} />
     </div>
   )
 }
