acf1d8fb044330adc3948164dbe0ff3730343deb
miri <49813832+mirilabs@users.noreply.github.com>
2025-01-23 20:16:23 +0000

    fix: end phase button calling handler twice

--- src/components/ControlPanel/ChapterMenu/index.tsx
+++ src/components/ControlPanel/ChapterMenu/index.tsx
@@ -1,20 +1,11 @@
-import React, { useContext }  from "react";
+import React from "react";
 import "./ChapterMenu.scss";
-import ThoronContext from "components/ThoronContext";
+import EndPhaseButton from "./EndPhaseButton";
 
 function ChapterMenu() {
-  // eslint-disable-next-line
-  const { controller } = useContext(ThoronContext);
-
-  const handleEndTurn = () => {
-    controller.pushAction({
-      command: "end_phase"
-    });
-  }
-
   return (
-    <div className="chapter-menu" onClick={handleEndTurn}>
-      <button onClick={handleEndTurn}>End Phase</button>
+    <div className="chapter-menu">
+      <EndPhaseButton />
     </div>
   )
 }
