42eec285e13d9914de65b35942dd2e0959f5ec28
miri <49813832+mirilabs@users.noreply.github.com>
2025-02-04 21:53:32 +0000

    add movetype icons

---
+++ src/icons/movetypes/index.ts
@@ -0,0 +1,16 @@
+
+import infantry from "./infantry.svg";
+import cavalry from "./cavalry.svg";
+import armor from "./armor.svg";
+import flying from "./flying.svg";
+import unknown from "../unknown.svg";
+
+const icons = {
+  infantry,
+  cavalry,
+  armor,
+  flying,
+  unknown
+}
+
+export default icons;
