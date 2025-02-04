c6daedf063cc0a2c28171ce8ac85a4a6a29d1cab
miri <49813832+mirilabs@users.noreply.github.com>
2025-02-04 19:32:18 +0000

    refactor: ThemeSelector to own file

--- src/components/ControlPanel/Settings/index.tsx
+++ src/components/ControlPanel/Settings/index.tsx
@@ -1,33 +1,13 @@
 import React from "react";
 import "./themes.scss";
-import settingsController, {
-  options,
-  Theme
-} from "./userSettings";
-
-function ThemeSelector({ settings, theme }: {
-  settings: typeof settingsController,
-  theme: Theme
-}) {
-  return (
-    <div className={`theme-selector theme-${theme}`}
-      onClick={() => settings.set("theme", theme)}>
-      {theme}
-    </div>
-  )
-}
+import ThemeSelector from "./ThemeSelector";
 
 function Settings() {
-  let settings = settingsController;
-
-  let themeSelectors = options.theme.map(theme => (
-    <ThemeSelector settings={settings} theme={theme} key={theme} />
-  ));
 
   return (
     <div className="settings">
       <h3>Theme</h3>
-      {themeSelectors}
+      <ThemeSelector />
     </div>
   );
 }
