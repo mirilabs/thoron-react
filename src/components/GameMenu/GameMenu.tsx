3d8f82cc1813614ff2030eebcab2ad5376d0e3fe
miri <49813832+mirilabs@users.noreply.github.com>
2025-02-11 23:22:54 -0500

    move end phase button to chapter log

--- src/components/GameMenu/GameMenu.tsx
+++ src/components/GameMenu/GameMenu.tsx
@@ -1,24 +1,33 @@
 import { useUIEmitter } from "components/utils/useUIAction";
 import "./GameMenu.scss";
-import React from 'react';
+import React, { useContext } from 'react';
+import ThoronContext from "components/ThoronContext";
 
 interface GameMenuProps {
   handleClose: () => void;
 }
 
 function GameMenu({ handleClose }: GameMenuProps) {
+  const { controller } = useContext(ThoronContext);
+
+  const endPhase = () => {
+    controller.pushAction({
+      command: "end_phase"
+    });
+  }
   const openLog = useUIEmitter("open_chapter_log");
   const openCharacter = useUIEmitter("open_character_detail");
   const openSettings = useUIEmitter("open_settings");
 
+  const handleEndPhase = () => { handleClose(); endPhase() }
   const handleOpenLog = () => { handleClose(); openLog() }
   const handleOpenCharacters = () => { handleClose(); openCharacter() }
   const handleOpenSettings = () => { handleClose(); openSettings() }
 
   return (
     <ul className="game-menu__options">
-      <li>
-        End Turn
+      <li onClick={handleEndPhase}>
+        End Phase
       </li>
       <li onClick={handleOpenLog}>
         Log
