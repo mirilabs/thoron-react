f0eb6605c0fd42362460e4080376c29bbd029d13
miri <49813832+mirilabs@users.noreply.github.com>
2025-04-27 19:16:16 -0400

    add material-ui overrides

---
+++ src/stylesheets/MUITheme.tsx
@@ -0,0 +1,45 @@
+import { createTheme } from "@mui/material";
+
+// https://mui.com/material-ui/customization/theme-components/
+
+const theme = createTheme({
+  components: {
+    MuiFormLabel: {
+      styleOverrides: {
+        root: {
+          color: "var(--text-color-2)",
+          fontFamily: "inherit",
+          "&.Mui-focused": {
+            color: "var(--accent-color)",
+          },
+        },
+      },
+    },
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+          color: "var(--text-color)",
+        }
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          "& .MuiOutlinedInput-notchedOutline": {
+            borderColor: "var(--bg-color-2)",
+          },
+          "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderColor: "var(--bg-color-3)",
+          },
+          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderColor: "var(--accent-color)",
+          }
+        },
+      }
+    },
+  }
+});
+
+console.log("MUI Theme", theme);
+
+export default theme;
