2118ff42c4d81c91773879b179e96f2716cb9376
mirilabs <49813832+mirilabs@users.noreply.github.com>
2025-04-28 06:23:13 +0000

    rename EndPhaseMotion to PhaseChangeMotion

--- src/game/entities/MotionSystem/PhaseChangeMotion.ts
+++ src/game/entities/MotionSystem/PhaseChangeMotion.ts
@@ -1,9 +1,13 @@
 import Game from "@/game/Game";
 import PhaseChange from "../ui/PhaseChange";
+import { ChapterEvent } from "thoron";
 
-function EndPhaseMotion(
+function PhaseChangeMotion(
   game: Game,
+  event: ChapterEvent
 ) {
+  console.log(event)
+
   return new Promise<void>((resolve) => {
     const phaseChange = new PhaseChange();
     phaseChange.addToScene(game.scene);
@@ -11,4 +15,4 @@ function EndPhaseMotion(
   });
 }
 
-export default EndPhaseMotion;
+export default PhaseChangeMotion;
