9cf5314dc74680e963f7c34fb76a04ba854ac370
miri <49813832+mirilabs@users.noreply.github.com>
2025-05-07 19:47:31 -0400

    feat: Statfield hides slider on small screens

--- src/app/ControlPanel/UnitEdit/StatField.tsx
+++ src/app/ControlPanel/UnitEdit/StatField.tsx
@@ -3,49 +3,56 @@ import NumberField from "./NumberField";
 import { Slider } from "@mui/material";
 import { IUnitRecord } from "thoron";
 import "./StatField.scss";
+import useResponsive, { WindowSize } from "@/app/utils/useResponsive";
 
 interface StatFieldProps {
   label: string;
   stat: string;
-  formData: IUnitRecord;
-  setFormData: (data: IUnitRecord) => void;
+  data: IUnitRecord;
+  setData: (data: IUnitRecord) => void;
 }
 
 function StatField({
   label,
   stat,
-  formData,
-  setFormData
+  data,
+  setData
 }: StatFieldProps) {
   const setStatValue = (stat: string, value: number) => {
-    setFormData({
-      ...formData,
+    setData({
+      ...data,
       stats: {
-        ...formData.stats,
+        ...data.stats,
         [stat]: value,
       },
     });
   };
+
+  const { size } = useResponsive();
+  const showSlider = size >= WindowSize.MEDIUM;
 
   return (
     <div className="stat-field">
       <NumberField
         label={label}
         name={stat}
-        value={formData.stats[stat]}
+        value={data.stats[stat]}
         onChange={(value) => setStatValue(stat, value)}
         min={0} max={99} step={1} />
-      <Slider
-        name={stat}
-        value={formData.stats[stat]}
-        onChange={(e, value) => setStatValue(stat, value as number)}
-        min={1} max={60} step={1}
-        valueLabelDisplay="auto"
-        sx={{
-          color: "var(--accent-color)",
-          width: "100px",
-          marginBottom: 1
-          }} />
+      {
+        showSlider &&
+        <Slider
+          name={stat}
+          value={data.stats[stat]}
+          onChange={(e, value) => setStatValue(stat, value as number)}
+          min={1} max={60} step={1}
+          valueLabelDisplay="auto"
+          sx={{
+            color: "var(--accent-color)",
+            width: "100px",
+            marginBottom: 1
+            }} />
+      }
     </div>
   );
 }
