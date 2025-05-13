0d04df2921992e25fd60b501d6c116a499cf502a
miri <49813832+mirilabs@users.noreply.github.com>
2025-05-13 19:33:48 +0000

    rename CombatPreview components for clarity

--- src/app/gameView/ActionMenu/CombatMenu/CombatPreview.tsx
+++ src/app/gameView/ActionMenu/CombatMenu/CombatPreview.tsx
@@ -1,88 +1,53 @@
-import "./CombatPreview.scss";
-import HPBar from "@/app/ControlPanel/UnitIndex/HPBar";
-import ItemIcon from "@/app/UnitSummary/ItemIcon";
 import React from "react";
-import { ItemRecord } from "thoron/dist/Item";
+import { ICombatForecast, DeployedUnit, ICombatForecastSide } from "thoron";
+import CombatPreviewSide, { ICombatPreview } from "./CombatPreviewSide";
 
-interface ICombatPreview {
-  unitName: string,       // name of unit
-  unitSpriteUrl?: string, // unit sprite url
-  equippedItem: ItemRecord, // equipped item
-  maxHP: number,      // max hp value
-  startHP: number,    // hp at beginning of combat
-  endHP: number,      // hp at end of combat
-  damage: number,     // damage value
-  doubles: boolean,   // true if speed high enough to double
-  hit: number,        // hit value
-  crit: number,       // crit value
-  isInRange: boolean  // true if unit able to attack
+function getForecastData(
+  unit: DeployedUnit,
+  forecast: ICombatForecastSide,
+  oppForecast: ICombatForecastSide
+): ICombatPreview {
+  const hp = unit.getState().hp;
+
+  return {
+    unitName: unit.record.name,
+    equippedItem: unit.items[unit.state.equippedIndex],
+    maxHP: unit.getStats().mhp,
+    startHP: hp,
+    endHP: hp - oppForecast.projectedTotalDamage,
+    ...(forecast as Required<ICombatForecastSide>)
+  }
 }
 
-function CombatPreview({ data }: {
-  data: ICombatPreview
+function CombatPreviewContainer({ attacker, target, forecast }: {
+  attacker: DeployedUnit,
+  target: DeployedUnit,
+  forecast: ICombatForecast
 }) {
-  const {
-    unitName,
-    unitSpriteUrl,
-    equippedItem,
-    maxHP, startHP, endHP,
-    damage, hit, crit, doubles,
-    isInRange
-  } = data;
+  const attData = getForecastData(
+    attacker,
+    forecast.initiator,
+    forecast.defender
+  );
+  const tgtData = getForecastData(
+    target,
+    forecast.defender,
+    forecast.initiator
+  )
 
   return (
-    <div className="combat-forecast">
-      <div>
-        <div className="name">
-          {
-            unitSpriteUrl &&
-            <img className="sprite" src={unitSpriteUrl} alt={unitName} />
-          }
-          {unitName}
-        </div>
-        <div className="equipped">
-          <ItemIcon item={equippedItem} />
-          <span>
-            {equippedItem.name}
-          </span>
-        </div>
-        <div className="hp">
-          <span className="label">HP</span>
-          <span className="value">
-            {startHP} → {endHP}
-          </span>
-        </div>
-        <HPBar maxHP={maxHP}
-          hp={startHP}
-          damage={startHP - endHP} />
-      </div>
-      <table className="stats">
-        <tbody>
-          <tr>
-            <th className="stat-label">Dmg</th>
-            <th className="stat-value">
-              {isInRange ? (damage + (doubles ? "×2" : "")) : "--"}
-            </th>
-          </tr>
-          <tr>
-            <th className="stat-label">Hit</th>
-            <th className="stat-value">
-              {isInRange ? hit : "--"}
-            </th>
-          </tr>
-          <tr>
-            <th className="stat-label">Crit</th>
-            <th className="stat-value">
-              {isInRange ? crit : "--"}
-            </th>
-          </tr>
-        </tbody>
-      </table>
+    <div className="combat-preview">
+      <span className="combat-preview__left">
+        <CombatPreviewSide data={attData} />
+      </span>
+      <span className="combat-preview__center">
+
+      </span>
+      <span className="combat-preview__right">
+        <CombatPreviewSide data={tgtData} />
+      </span>
     </div>
   )
 }
 
-export default CombatPreview;
-export {
-  ICombatPreview
-}
+export default CombatPreviewContainer;
