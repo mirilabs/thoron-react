9b5d3b8c98924ecc6610f0cd477cbd4337c05be0
miri <49813832+mirilabs@users.noreply.github.com>
2025-05-13 18:42:12 +0000

    refactor: change CombatStartHandler to ActionConfirmHandler (for all actions)

--- src/app/gameView/ActionMenu/index.tsx
+++ src/app/gameView/ActionMenu/index.tsx
@@ -3,18 +3,19 @@ import ActionMenu from "./ActionSelectMenu";
 import { useControllerSelector } from "@/app/utils/reduxHooks";
 import { ControllerPhase } from "@/game/entities/ControlSystem/ControllerState";
 import CombatMenu from "../CombatMenu";
+import ActionConfirmHandler from "./ActionConfirmHandler";
 
 function ActionUI() {
   const phase = useControllerSelector(state => state.phase);
   const action = useControllerSelector(state => state.pendingMove.action);
+
+  const { ACTION_SELECT, ACTION_CONFIRM } = ControllerPhase;
   
   return (
     <>
-      <ActionMenu display={phase === ControllerPhase.ACTION_SELECT} />
-      <CombatMenu display={
-        phase === ControllerPhase.ACTION_CONFIRM &&
-        action === "attack"
-        } />
+      <ActionMenu display={phase === ACTION_SELECT} />
+      <ActionConfirmHandler enabled={phase === ACTION_CONFIRM} />
+      <CombatMenu display={phase === ACTION_CONFIRM && action === "attack"} />
     </>
   )
 }
