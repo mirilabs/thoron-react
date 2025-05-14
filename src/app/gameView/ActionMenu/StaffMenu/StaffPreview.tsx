a545b116693f26a0a39690e73d1991174236a8dc
miri <49813832+mirilabs@users.noreply.github.com>
2025-05-14 22:27:34 +0000

    add staff preview panel

--- src/app/gameView/ActionMenu/StaffMenu/StaffPreview.tsx
+++ src/app/gameView/ActionMenu/StaffMenu/StaffPreview.tsx
@@ -1,6 +1,6 @@
 import ThoronContext from '@/app/ThoronContext';
 import React, { useContext, useEffect } from 'react';
-import { DeployedUnit, ItemRecord } from 'thoron';
+import { DeployedUnit, ItemRecord, staffEffects } from 'thoron';
 
 interface StaffPreviewProps {
   unit: DeployedUnit;
@@ -9,19 +9,37 @@ interface StaffPreviewProps {
 }
 
 function StaffPreview({ unit, target, staff }: StaffPreviewProps) {
-  const { controller } = useContext(ThoronContext);
-
-  useEffect(() => {
-    if (!unit || !target || !staff) return;
-
-    // get staff effect preview
-    // TODO
-  }, [unit, target, staff, controller]);
+  if (!unit || !target || !staff || !(staff.type === "staff"))
+    return <div>No staff selected</div>;
 
   return (
     <div className="staff-preview">
+      <StaffPreviewContent unit={unit} target={target} staff={staff} />
     </div>
   )
+}
+
+function StaffPreviewContent({ unit, target, staff }: StaffPreviewProps) {
+  if (!unit || !target || !staff || !(staff.type === "staff"))
+    return <div>No staff selected</div>;
+
+  switch (staff.stats.effect) {
+    case "heal":
+      const amount = staffEffects.heal.getHealValue(staff, unit, target);
+      return (
+        <div>
+          <p>Heal</p>
+          <p>{target.record.name}</p>
+          <p>{target.hp} → {target.hp + amount}</p>
+        </div>
+      );
+    default:
+      return (
+        <div>
+          <p>Unknown staff effect</p>
+        </div>
+      );
+  }
 }
 
 export default StaffPreview;
