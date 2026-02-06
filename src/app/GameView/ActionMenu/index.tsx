1b7bf402a0bfa6dac78d84456e38f4fe3352a0d3
mirilabs <49813832+mirilabs@users.noreply.github.com>
2026-02-05 19:15:45 -0500

    feat: add trade menu (cosmetic)

--- src/app/GameView/ActionMenu/index.tsx
+++ src/app/GameView/ActionMenu/index.tsx
@@ -5,6 +5,7 @@ import { ControllerPhase } from "@/game/entities/ControlSystem/ControllerState";
 import CombatMenu from "./CombatMenu";
 import ActionConfirmHandler from "./ActionConfirmHandler";
 import StaffMenu from "./StaffMenu";
+import TradeMenu from "./TradeMenu";
 import "./ActionConfirmMenu.scss";
 
 function ActionUI() {
@@ -12,13 +13,14 @@ function ActionUI() {
   const action = useControllerSelector(state => state.pendingMove.action);
 
   const { ACTION_SELECT, ACTION_CONFIRM } = ControllerPhase;
-  
+
   return (
     <>
       <ActionSelectMenu display={phase === ACTION_SELECT} />
       <ActionConfirmHandler enabled={phase === ACTION_CONFIRM} />
       <CombatMenu display={phase === ACTION_CONFIRM && action === "attack"} />
       <StaffMenu display={phase === ACTION_CONFIRM && action === "staff"} />
+      <TradeMenu display={phase === ACTION_CONFIRM && action === "trade"} />
     </>
   )
 }
