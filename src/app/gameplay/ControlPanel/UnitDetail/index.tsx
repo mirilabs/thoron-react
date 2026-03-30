f0892385bbe08272c475bc318749fd70c9a4aec7
mirilabs <49813832+mirilabs@users.noreply.github.com>
2026-03-29 23:39:03 -0400

    fix UnitEditForm not updating unit record

--- src/app/gameplay/ControlPanel/UnitDetail/index.tsx
+++ src/app/gameplay/ControlPanel/UnitDetail/index.tsx
@@ -2,13 +2,19 @@ import React, { useState } from "react";
 import UnitDetail from "./UnitDetail";
 import { DeployedUnit } from "thoron";
 import UnitEditForm from "../UnitEdit/UnitEditForm";
+import { IUnitRecord } from "thoron";
 
 function UnitDetailContainer({ unit }: { unit: DeployedUnit }) {
   const [editing, setEditing] = useState(false);
 
+  const handleSave = (record: IUnitRecord) => {
+    unit.record = record;
+    setEditing(false);
+  }
+
   return editing ? (
-    <UnitEditForm unit={unit}
-      handleSave={() => setEditing(false)}
+    <UnitEditForm record={unit.record}
+      handleSave={handleSave}
       handleCancel={() => setEditing(false)} />
   ) : (
     <UnitDetail unit={unit} handleStartEdit={() => setEditing(true)} />
