e198ce8965837d46e7e8782707938fa9646756d9
miri <49813832+mirilabs@users.noreply.github.com>
2026-03-30 16:25:52 -0400

    implement CharacterIndex

--- src/app/characters/CharacterCard.tsx
+++ src/app/characters/CharacterCard.tsx
@@ -4,9 +4,24 @@ import React from "react";
 function CharacterCard({ character }: {
   character: Character
 }) {
+  const portraitUrl = character.portrait ?
+    URL.createObjectURL(character.portrait) : "";
+  const mapSpriteUrl = character.mapSprite ?
+    URL.createObjectURL(character.mapSprite) : "";
+
   return (
-    <div className="border border-gray-500 rounded-lg p-4 min-w-[320px]">
-      <h1>{character.name}</h1>
+    <div className={
+      "border border-gray-500 rounded-lg p-4 min-w-[320px] " +
+      "flex flex-row items-center gap-4"
+    }>
+      <img src={portraitUrl} alt={character.name} width={64} height={64} />
+      <div className="flex flex-col">
+        <h1 className="text-xl font-bold">{character.name}</h1>
+        <p>Level {character.level} {character.className}</p>
+      </div>
+      <img src={mapSpriteUrl} alt={character.name}
+        className="ml-auto"
+        width={32} height={32} />
     </div>
   )
 }
