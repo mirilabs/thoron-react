5bf3ef9e6f2478efb863e7776ffa28f766b99537
miri <49813832+mirilabs@users.noreply.github.com>
2026-03-29 23:19:08 -0400

    add CampaignDetail

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Navigation from "./Navigation";
 import CampaignIndex from "./app/campaigns/CampaignIndex";
+import CampaignDetail from "./app/campaigns/CampaignDetail";
 import GameplayRoot from "./app/gameplay/GameplayRoot";
 
 export default function Router() {
@@ -10,6 +11,7 @@ export default function Router() {
       <div className="content">
         <Routes>
           <Route path="/" element={<CampaignIndex />} />
+          <Route path="/campaigns/:id" element={<CampaignDetail />} />
           <Route path="/gameplay" element={<GameplayRoot />} />
         </Routes>
       </div>
---
+++ src/app/campaigns/CampaignDetail.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import db from "@/data/db";
+import { useLiveQuery } from "dexie-react-hooks";
+import { useParams } from "react-router";
+
+function CampaignDetail() {
+  const { id } = useParams();
+  const campaign = useLiveQuery(() => db.campaigns.get(Number(id)), [id]);
+
+  if (!campaign) return <div>Campaign not found</div>;
+
+  return (
+    <div className="h-full flex flex-col items-center">
+      <div className="border border-gray-500 rounded-lg p-4 w-[80%]">
+        <h1 className="text-xl font-semibold">{campaign.title}</h1>
+      </div>
+    </div>
+  )
+}
+
+export default CampaignDetail;
