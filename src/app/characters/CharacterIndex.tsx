d35a1646c8d026f0e4d66afa2641a6d1eabd5055
mirilabs <49813832+mirilabs@users.noreply.github.com>
2026-03-30 16:25:52 -0400

    implement CharacterIndex

--- src/app/characters/CharacterIndex.tsx
+++ src/app/characters/CharacterIndex.tsx
@@ -8,14 +8,15 @@ function CharacterIndex({ campaignId }: { campaignId: number }) {
   const characters = useLiveQuery(() => db.characters.where({ campaignId }).toArray(), [campaignId]);
 
   return (
-    <div className="flex flex-col gap-4 border border-[var(--text-color)] rounded-lg p-4 m-4 bg-[var(--bg-color)]">
+    <div className={
+      "flex flex-col gap-4 border border-[var(--text-color)] rounded-lg p-4 m-4 " +
+      "bg-[var(--bg-color)]"
+    }>
       <h1 className="text-xl font-bold">Characters</h1>
-      <div className="flex flex-wrap gap-4">
-        {characters?.map((character) => (
-          <CharacterCard key={character.id} character={character} />
-        ))}
-        <CharacterCreate campaignId={campaignId} />
-      </div>
+      {characters?.map((character) => (
+        <CharacterCard key={character.id} character={character} />
+      ))}
+      <CharacterCreate campaignId={campaignId} />
     </div>
   )
 }
