90c03d892f19276dcc379c9c772eb00d586087d6
mirilabs <49813832+mirilabs@users.noreply.github.com>
2026-04-12 15:02:03 -0400

    move GameplayLayout to own component

---
+++ src/app/gameplay/GameplayLayout.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./GameplayLayout.scss";
+import GameCanvas from "./GameView/GameCanvas";
+import ActionUI from "./GameView/ActionMenu";
+import UnitSummary from "./UnitSummary";
+import GameMenuContainer from "./GameMenu";
+import ControlPanelContainer from "./ControlPanel";
+import useResponsive, { WindowSize } from "./utils/useResponsive";
+
+function SmallLayout() {
+  return (
+    <div className="layout-mobile">
+      <div className="gui-layer">
+        <UnitSummary />
+      </div>
+      <div className="gui-layer">
+        <GameMenuContainer />
+      </div>
+      <div className="gui-layer">
+        <ControlPanelContainer />
+      </div>
+      <div className="gui-layer">
+        <ActionUI />
+      </div>
+      <GameCanvas />
+    </div>
+  )
+}
+
+function LargeLayout() {
+  return (
+    <div className="layout-mobile">
+      <div className="gui-layer">
+        <UnitSummary />
+      </div>
+      <div className="gui-layer">
+        <GameMenuContainer />
+      </div>
+      <div className="gui-layer">
+        <ControlPanelContainer />
+      </div>
+      <div className="gui-layer">
+        <ActionUI />
+      </div>
+      <GameCanvas />
+    </div>
+  )
+}
+
+function GameplayLayout() {
+  const { size } = useResponsive();
+
+  switch (size) {
+    case WindowSize.SMALL:
+      return <SmallLayout />;
+    case WindowSize.LARGE:
+      return <LargeLayout />;
+    default:
+      return <SmallLayout />;
+  }
+}
+
+export default GameplayLayout;
--- src/app/gameplay/GameplayRoot.tsx
+++ src/app/gameplay/GameplayRoot.tsx
@@ -1,14 +1,7 @@
-import "./GameplayRoot.scss";
 import React from "react";
-import useResponsive from "./utils/useResponsive";
-import { WindowSize } from "./ViewportContext";
 
-// COMPONENTS
-import GameCanvas from './GameView/GameCanvas';
-import ActionUI from "./GameView/ActionMenu";
-import UnitSummary from "./UnitSummary";
-import GameMenuContainer from "./GameMenu";
-import ControlPanelContainer from "./ControlPanel";
+import GameplayLayout from "./GameplayLayout";
+import { SaveState } from "thoron";
 
 // CONTEXTS
 import { ThoronProvider } from "./ThoronContext";
@@ -16,40 +9,12 @@ import { Provider as ReduxProvider } from "react-redux";
 import controllerStore from "@/shared/store";
 import { KeybindInitializer } from "./ControlPanel/Settings/keybinds";
 
-import { SaveState } from "thoron";
-
-function Layout() {
-  let { size } = useResponsive();
-
-  switch (size) {
-    case WindowSize.SMALL:
-    default:
-      return (
-        <div className="layout-mobile">
-          <div className="gui-layer">
-            <UnitSummary />
-          </div>
-          <div className="gui-layer">
-            <GameMenuContainer />
-          </div>
-          <div className="gui-layer">
-            <ControlPanelContainer />
-          </div>
-          <div className="gui-layer">
-            <ActionUI />
-          </div>
-          <GameCanvas />
-        </div>
-      );
-  }
-}
-
 function GameplayRoot({ saveState }: { saveState: SaveState }) {
   return (
     <ThoronProvider saveState={{ ...saveState }}>
       <ReduxProvider store={controllerStore}>
         <div className="gameplay-root">
-          <Layout />
+          <GameplayLayout />
         </div>
         <> {/* config */}
           <KeybindInitializer />
