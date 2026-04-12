9f15a3578fed8e3907cd6b1ab6a94c9fb05e8b94
mirilabs <49813832+mirilabs@users.noreply.github.com>
2026-04-12 15:59:14 -0400

    replace legacy ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,19 @@ import "./stylesheets/dev.css";
 import "./stylesheets/themes.scss";
 
 import React from "react";
-import { ErrorBoundary } from "react-error-boundary";
-import fallbackRender from "@/app/gameplay/utils/fallbackRender";
 import { ViewportProvider } from "@/app/gameplay/ViewportContext";
 import { initializeUserSettings } from "@/app/gameplay/ControlPanel/Settings/userSettings";
 import { ThemeProvider } from "@mui/material";
 import MUITheme from "@/stylesheets/MUITheme";
 import Router from "./Router";
 import Alerts from "./app/core/Alerts";
+import ErrorBoundary from "./app/core/ErrorBoundary";
 
 initializeUserSettings();
 
 export default function App() {
   return (
-    <ErrorBoundary fallbackRender={fallbackRender}>
+    <ErrorBoundary>
       <ViewportProvider>
         <ThemeProvider theme={MUITheme}>
           <Router />
