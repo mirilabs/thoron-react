378b0d4f2b3d09e37918c830509b716a0402dc04
mirilabs <49813832+mirilabs@users.noreply.github.com>
2026-04-12 09:56:19 -0400

    feat: ChapterList and creation

---
+++ src/app/chapters/ChapterCreate.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import db from "@/data/db";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  TextField,
+  IconButton
+} from "@mui/material";
+import { useNavigate } from "react-router";
+
+function ChapterCreate({ campaignId }: { campaignId: number }) {
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const chapterId = await db.chapters.add({
+      campaignId,
+      name,
+    });
+    setName("");
+    setOpen(false);
+    navigate(`/campaigns/${campaignId}/chapters/${chapterId}`);
+  };
+
+  return (
+    <>
+      <Button variant="contained" onClick={() => setOpen(true)}
+        startIcon={<i className="fas fa-plus" />}>
+        New Chapter
+      </Button>
+      {open &&
+        <Dialog open={open} onClose={() => setOpen(false)}>
+          <DialogTitle>Create Chapter</DialogTitle>
+          <DialogContent style={{ paddingTop: "4px" }}>
+            <TextField
+              label="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setOpen(false)}>Cancel</Button>
+            <Button onClick={handleSubmit}>Create</Button>
+          </DialogActions>
+        </Dialog>
+      }
+    </>
+  )
+}
+
+export default ChapterCreate;
---
+++ src/app/chapters/ChapterList.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useLiveQuery } from "dexie-react-hooks";
+import db, { type Chapter } from "@/data/db";
+import ChapterCreate from "./ChapterCreate";
+import { Link } from "react-router";
+
+function ChapterCard({ chapter }: { chapter: Chapter }) {
+  return (
+    <Link to={`/campaigns/${chapter.campaignId}/chapters/${chapter.id}`}>
+      <div
+        className={
+          "bg-[var(--bg-color)] border border-[var(--text-color)] " +
+          "rounded-lg p-4 " +
+          "flex flex-col"
+        }>
+        <h1 className="text-xl font-bold">{chapter.name}</h1>
+      </div>
+    </Link>
+  )
+}
+
+function ChapterList({ campaignId }: { campaignId: number }) {
+  const chapters = useLiveQuery(
+    () => db.chapters.where({ campaignId }).toArray(),
+    [campaignId]
+  );
+
+  return (
+    <div className={
+      "bg-[var(--bg-color)] border border-[var(--text-color)] " +
+      "rounded-lg p-4 " +
+      "flex flex-col gap-4"
+    }>
+      <h1 className="text-xl font-bold">Chapters</h1>
+      {chapters?.map((chapter) => (
+        <ChapterCard key={chapter.id} chapter={chapter} />
+      ))}
+      <ChapterCreate campaignId={campaignId} />
+    </div>
+  )
+}
+
+export default ChapterList;
