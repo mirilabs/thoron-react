b3fbe6f21cafd32b91ebd1ac11e0741d56880636
miri <49813832+mirilabs@users.noreply.github.com>
2026-04-13 00:41:37 -0400

    update GameLayout to handle window size changes more gracefully

--- src/app/gameplay/GameplayLayout.tsx
+++ src/app/gameplay/GameplayLayout.tsx
@@ -7,34 +7,13 @@ import GameMenuContainer from "./GameMenu";
 import ControlPanelContainer from "./ControlPanel";
 import useResponsive, { WindowSize } from "../utils/useResponsive";
 
-function SmallLayout() {
-  return (
-    <div className="layout-mobile">
-      <div className="gui-layer">
-        <UnitSummary />
-      </div>
-      <div className="gui-layer">
-        <GameMenuContainer />
-      </div>
-      <div className="gui-layer">
-        <ControlPanelContainer />
-      </div>
-      <div className="gui-layer">
-        <ActionUI />
-      </div>
-      <GameCanvas />
-    </div>
-  )
-}
+function GameplayLayout() {
+  const { size } = useResponsive();
 
-function LargeLayout() {
   return (
     <div className="flex flex-row w-full h-full">
       {/* Game View Side */}
-      <div className={
-        "flex-1 relative h-full overflow-hidden " +
-        "border-r border-[var(--text-color-2)]/20"
-      }>
+      <div className="flex-1 relative h-full overflow-hidden">
         <div className="gui-layer">
           <UnitSummary />
         </div>
@@ -44,31 +23,28 @@ function LargeLayout() {
         <div className="gui-layer">
           <ActionUI />
         </div>
+        {
+          size <= WindowSize.MEDIUM ? (
+            <div className="gui-layer">
+              <ControlPanelContainer />
+            </div>
+          ) : null
+        }
         <GameCanvas />
       </div>
-
       {/* Control Panel Side */}
-      <div className={
-        "h-full overflow-hidden " +
-        "w-[33%] min-w-[320px] max-w-[480px]"
-      }>
-        <ControlPanelContainer variant="static" />
-      </div>
+      {
+        size >= WindowSize.LARGE ? (
+          <div className={
+            "h-full overflow-hidden " +
+            "w-[33%] min-w-[320px] max-w-[480px]"
+          }>
+            <ControlPanelContainer variant="static" />
+          </div>
+        ) : null
+      }
     </div>
   )
-}
-
-function GameplayLayout() {
-  const { size } = useResponsive();
-
-  switch (size) {
-    case WindowSize.SMALL:
-      return <SmallLayout />;
-    case WindowSize.LARGE:
-      return <LargeLayout />;
-    default:
-      return <SmallLayout />;
-  }
 }
 
 export default GameplayLayout;
