c0d5235bf1d0edbc90425606352315acec2bd417
mirilabs <49813832+mirilabs@users.noreply.github.com>
2026-04-13 03:10:38 -0400

    refactor: CampaignDetail layout

--- src/app/campaigns/CampaignDetail.tsx
+++ src/app/campaigns/CampaignDetail.tsx
@@ -7,6 +7,7 @@ import CampaignEdit from "./CampaignEdit";
 import { Button, IconButton } from "@mui/material";
 import { Campaign } from "@/data/db";
 import ChapterList from "../chapters/ChapterList";
+import CampaignAssets from "./CampaignAssets";
 
 function CampaignInfo({ campaign }: { campaign: Campaign }) {
   const [isEditing, setIsEditing] = useState(false);
@@ -43,16 +44,10 @@ function CampaignInfo({ campaign }: { campaign: Campaign }) {
 
 function CampaignDetail() {
   const { id } = useParams();
-  const campaign = useLiveQuery(() => db.campaigns.get(Number(id)), [id]);
-
-  const mapCount = useLiveQuery(
-    () => db.maps.where({ campaignId: Number(id) }).count(),
-    [id]
-  );
-
-  const itemCount = useLiveQuery(
-    () => db.items.where({ campaignId: Number(id) }).count(),
-    [id]
+  const campaignId = Number(id);
+  const campaign = useLiveQuery(
+    () => db.campaigns.get(campaignId),
+    [campaignId]
   );
 
   if (!campaign) return (
@@ -64,40 +59,10 @@ function CampaignDetail() {
   return (
     <div className="h-full flex flex-col items-center gap-4">
       <CampaignInfo campaign={campaign} />
-      <ChapterList campaignId={Number(id)} />
-      <CharacterList campaignId={Number(id)} />
-      <div className={
-        "bg-[var(--bg-color)] border border-[var(--text-color)] " +
-        "rounded-lg p-4 " +
-        "flex flex-col"
-      }>
-        <h1 className="text-xl font-bold">Assets</h1>
-        <div className="flex flex-row items-center justify-between gap-8">
-          <Link to={`/campaigns/${id}/maps`}>
-            <Button variant="text"
-              startIcon={<i className="fas fa-map" />}>
-              <p className="text-lg font-semibold">
-                Maps
-              </p>
-            </Button>
-          </Link>
-          <p className="text-lg font-semibold">
-            {mapCount}
-          </p>
-        </div>
-        <div className="flex flex-row items-center justify-between gap-8">
-          <Link to={`/campaigns/${id}/items`}>
-            <Button variant="text"
-              startIcon={<i className="fas fa-toolbox" />}>
-              <p className="text-lg font-semibold">
-                Items
-              </p>
-            </Button>
-          </Link>
-          <p className="text-lg font-semibold">
-            {itemCount}
-          </p>
-        </div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-4">
+        <ChapterList campaignId={campaignId} />
+        <CharacterList campaignId={campaignId} />
+        <CampaignAssets campaignId={campaignId} />
       </div>
     </div>
   )
