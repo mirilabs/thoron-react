9047ecbfcd4cd713ea132779bdf656fc577381ca
miri <49813832+mirilabs@users.noreply.github.com>
2026-04-13 22:11:26 -0400

    feat: campaign/:id/characters index route

--- src/app/characters/CharacterShow.tsx
+++ src/app/characters/CharacterShow.tsx
@@ -1,32 +1,19 @@
 import React from "react";
 import { useLiveQuery } from "dexie-react-hooks";
-import { Link, useParams } from "react-router";
 import db from "@/data/db";
-import CharacterList from "./CharacterList";
 import CharacterDetail from "./CharacterDetail";
-import { Button } from "@mui/material";
 
-function CharacterShow() {
-  const { id: campaignId, characterId } = useParams();
+function CharacterShow({ characterId }: { characterId: number }) {
+  const character = useLiveQuery(
+    () => db.characters.get(characterId), [characterId]);
 
-  const character = useLiveQuery(() => (
-    db.characters.get(Number(characterId))
-  ), [characterId]);
+  if (!character) {
+    return <div>Character not found</div>
+  }
 
   return (
-    <div className="flex flex-row gap-4">
-      <div className="flex flex-col gap-4 p-4">
-        <Link to={`/campaigns/${campaignId}`}>
-          <Button variant="text"
-            startIcon={<i className="fas fa-arrow-left" />}>
-            Back
-          </Button>
-        </Link>
-        <CharacterList campaignId={Number(campaignId)} />
-      </div>
-      <CharacterDetail character={character} editable={true} />
-    </div>
+    <CharacterDetail character={character} editable={true} />
   )
 }
 
-export default CharacterShow
+export default CharacterShow;
