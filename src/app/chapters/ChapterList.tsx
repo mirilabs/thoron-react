f6bea6886232a9b9f3f93f1f69870c4be11edb8b
mirilabs <49813832+mirilabs@users.noreply.github.com>
2026-04-13 22:22:37 -0400

    improve ChapterCard styling

--- src/app/chapters/ChapterList.tsx
+++ src/app/chapters/ChapterList.tsx
@@ -1,19 +1,39 @@
 import React from "react";
 import { useLiveQuery } from "dexie-react-hooks";
-import db, { type Chapter } from "@/data/db";
+import db, { Map, type Chapter } from "@/data/db";
 import ChapterCreate from "./ChapterCreate";
 import { Link } from "react-router";
 
 function ChapterCard({ chapter }: { chapter: Chapter }) {
+  const { save } = chapter;
+  const background = (save.map as Map).background;
+  const backgroundUrl = background ? URL.createObjectURL(background) : "";
+
   return (
     <Link to={`/campaigns/${chapter.campaignId}/chapters/${chapter.id}`}>
       <div
         className={
-          "bg-[var(--bg-color)] border border-[var(--text-color)] " +
+          "bg-[var(--bg-color-2)] border-[var(--text-color-2)] border " +
+          "hover:bg-[var(--bg-color)] hover:border-[var(--text-color)] " +
+          "transition-colors duration-200 " +
           "rounded-lg p-4 " +
-          "flex flex-col"
+          "flex flex-row gap-4"
         }>
-        <h1 className="text-xl font-bold">{chapter.name}</h1>
+        <div className="w-32 h-32">
+          {
+            backgroundUrl && (
+              <img src={backgroundUrl} alt=""
+                className="w-full h-full object-contain rounded-lg"
+              />
+            )
+          }
+        </div>
+        <div className="flex flex-col">
+          <h1 className="text-xl font-bold">{chapter.name}</h1>
+          <p className="text-sm text-[var(--text-color-2)]">
+            Turn {save.turn}
+          </p>
+        </div>
       </div>
     </Link>
   )
