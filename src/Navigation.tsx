715e4453fc5c9e72bebd44d339137a630e1e0363
mirilabs <49813832+mirilabs@users.noreply.github.com>
2026-04-15 01:40:23 -0400

    feat: responsive mobile nav bar

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,7 +1,7 @@
 import { Link, useLocation } from "react-router";
 import "./Navigation.scss";
 import React from "react";
-import { Button } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 import SettingsRoot from "./app/settings/SettingsRoot";
 import { useLiveQuery } from "dexie-react-hooks";
 import db from "./data/db";
@@ -9,15 +9,27 @@ import db from "./data/db";
 function CampaignHomeNav({ campaignId }: { campaignId: number }) {
   const campaign = useLiveQuery(() => db.campaigns.get(campaignId));
   if (!campaign) return null;
+
+  const baseClassName = (
+    "hover:bg-[var(--bg-color-2)] transition-colors duration-200 items-center "
+  );
   return (
-    <Link to={`/campaigns/${campaignId}`} className={
-      "p-4 flex items-center " +
-      "hover:bg-[var(--bg-color-2)] transition-colors duration-200 "
-    }>
-      <h2 className="text-xl text-[var(--text-color-2)] max-w-[200px] truncate">
-        {campaign.title}
-      </h2>
-    </Link>
+    <>
+      <Link to={`/campaigns/${campaignId}`} className={
+        baseClassName + "hidden sm:flex p-4"
+      }>
+        <h2 className="text-xl text-[var(--text-color-2)] max-w-[200px] truncate">
+          {campaign.title}
+        </h2>
+      </Link>
+      <Link to={`/campaigns/${campaignId}`} className={
+        baseClassName + "flex sm:hidden p-2"
+      }>
+        <IconButton>
+          <i className="fas fa-home" />
+        </IconButton>
+      </Link>
+    </>
   )
 }
 
@@ -29,13 +41,20 @@ function NavButton({ to, label, icon }: {
   const iconElement = icon ? <i className={`fas fa-${icon}`} /> : null;
 
   return (
-    <Link to={to} className="nav-link">
-      <Button variant="text"
-        sx={{ flex: 1, flexDirection: "column", gap: 0.5, py: 1 }}>
-        {iconElement}
-        {label}
-      </Button>
-    </Link>
+    <>
+      <Link to={to} className="hidden sm:flex">
+        <Button variant="text"
+          sx={{ flex: 1, flexDirection: "column", gap: 0.5, py: 1 }}>
+          {iconElement}
+          {label}
+        </Button>
+      </Link>
+      <Link to={to} className="flex sm:hidden items-center p-2">
+        <IconButton>
+          {iconElement}
+        </IconButton>
+      </Link>
+    </>
   )
 }
 
@@ -47,10 +66,10 @@ function Navigation() {
   return (
     <nav className="flex flex-row gap-1">
       <Link to={"/"} className={
-        "ml-4 p-4 flex items-center " +
+        "ml-2 sm:ml-4 p-2 sm:p-4 flex items-center " +
         "hover:bg-[var(--bg-color-2)] transition-colors duration-200 "
       }>
-        <h1 className="text-2xl font-bold">thoron</h1>
+        <h1 className="text-lg sm:text-2xl font-bold">thoron</h1>
       </Link>
       {campaignId &&
         <>
