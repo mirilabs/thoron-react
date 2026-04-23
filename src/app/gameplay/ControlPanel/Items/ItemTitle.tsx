8d60cd047f31a103f7551933aad2b218a2bb2f99
miri <49813832+mirilabs@users.noreply.github.com>
2026-04-23 00:50:56 -0400

    refactor ItemCard styles

--- src/app/gameplay/ControlPanel/Items/ItemTitle.tsx
+++ src/app/gameplay/ControlPanel/Items/ItemTitle.tsx
@@ -1,4 +1,3 @@
-import "./ItemTitle.scss";
 import React from 'react';
 import { ItemRecord } from 'thoron';
 import ItemIcon from './ItemIcon';
@@ -7,21 +6,28 @@ interface ItemTitleProps extends React.HTMLAttributes<HTMLDivElement> {
   item: ItemRecord;
 }
 
-function ItemTitle({ item, ...props }: ItemTitleProps) {
-  const className = (props.onClick !== undefined) ?
-    "item-title clickable" :
-    "item-title";
+function ItemTitle({ item, className, ...props }: ItemTitleProps) {
+  const hasOnClick = props.onClick !== undefined;
+  const classNames = [
+    "min-w-[120px] h-[32px] rounded-md bg-[var(--bg-color-2)]",
+    "flex flex-row items-center flex-wrap gap-2",
+    hasOnClick ? "cursor-pointer" : "cursor-default",
+    hasOnClick ? "hover:text-shadow-[0px_0px_3px_var(--text-color)]" : "",
+    className
+  ].join(" ");
 
   let { uses, maxUses } = item;
   if (maxUses && uses === undefined) uses = maxUses;
 
   return (
-    <div className={className} {...props}>
+    <div className={classNames} {...props}>
       <ItemIcon item={item} />
-      <h2 className="name">{item.name ?? '???'}</h2>
+      <h2 className="text-md text-[var(--text-color)] text-shadow-inherit">
+        {item.name ?? '???'}
+      </h2>
       {
         (maxUses) && (
-          <span className="uses">
+          <span className="text-sm text-[var(--text-color-2)]">
             {uses} / {maxUses}
           </span>
         )
