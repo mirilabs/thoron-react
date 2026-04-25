707d3df1136de1a0dbb1ee00d8c43c7c06461443
miri <49813832+mirilabs@users.noreply.github.com>
2026-04-24 22:46:35 -0400

    refactor: move AttackEvent parsing from Damagenumber to AttackMotion

--- src/game/entities/MotionSystem/AttackMotion.ts
+++ src/game/entities/MotionSystem/AttackMotion.ts
@@ -11,6 +11,26 @@ const FORWARD_MOTION_TIME = 120;  // ms
 const BACKWARD_MOTION_TIME = 80;  // ms
 const POST_ATTACK_DELAY_TIME = 500;  // ms
 
+function createDamageNumber(game: Game, target: UnitBody, event: AttackEvent) {
+  let origin = Vector2.scale({
+    x: game.config.tileWidth,
+    y: game.config.tileHeight
+  }, 0.5);
+  origin = origin.sum(target.entity.getComponent("position"));
+
+  let value: string | number = "";
+  if (event.didHit) {
+    if (event.didCrit) value = `CRIT ${event.damage}`;
+    else value = event.damage;
+  }
+  else {
+    value = "MISS";
+  }
+
+  const damageNumber = new DamageNumber(origin, value, game.config);
+  damageNumber.addToScene(game.scene);
+}
+
 function AttackMotion(
   game: Game,
   attacker: UnitBody,
@@ -34,20 +54,13 @@ function AttackMotion(
   return async function attackMotion() {
     // create damage number when forward motion is done
     window.setTimeout(() => {
-      let origin = Vector2.scale({
-        x: game.config.tileWidth,
-        y: game.config.tileHeight
-      }, 0.5);
-      origin = origin.sum(target.entity.getComponent("position"));
-
-      const damageNumber = new DamageNumber(origin, event, game.config);
-      damageNumber.addToScene(game.scene);
+      createDamageNumber(game, target, event);
     }, FORWARD_MOTION_TIME);
 
     // start motion
     await path.start();
     attacker.resetPosition();
-    
+
     // wait a bit before system proceeds to next motion
     await wait(POST_ATTACK_DELAY_TIME);
   }
