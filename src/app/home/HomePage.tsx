70ac58f8e676a1c6a2bc02bc7a353fe5a58511cd
miri <49813832+mirilabs@users.noreply.github.com>
2026-04-25 20:02:39 -0400

    add scrolling buffer to mobile homepage

--- src/app/home/HomePage.tsx
+++ src/app/home/HomePage.tsx
@@ -7,17 +7,15 @@ export default function HomePage() {
   const [size, sizes] = useResponsive();
 
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <div className={"flex gap-4 overflow-y-scroll " +
-        (size === sizes.SMALL ? "flex-col" : "flex-row")
-      }>
-        <div className="p-2">
-          <About />
-        </div>
-        <div className="p-2">
-          <CampaignIndex />
-        </div>
-      </div>
+    <div className={
+      "w-full md:h-full " +
+      "flex gap-4 items-center " +
+      "flex-col md:flex-row " +
+      "pt-16 pb-32 md:py-0 " +
+      "justify-center"
+    }>
+      <About />
+      <CampaignIndex />
     </div>
   )
 }
